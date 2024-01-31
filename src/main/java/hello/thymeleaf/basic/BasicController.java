d7d3a9d5ad2bdf8b53ad390280c15aaea19915a8
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-31 19:01:59 +0900

    1. 타임리프 기본 기능 | URL 링크 단순한 URL @{...} - @{...} 쿼리 파라미터 () - @{...}(param1=${data}) 경로 변수 {} - @{...}{pathVariable}(pathVariable=${data}) 경로 변수 + 쿼리 파라미터 {}() - @{...}{pathVariable}(pathVariable=${data1}, param1=${data2})

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -74,6 +74,13 @@ public class BasicController {
         return "basic/date.html";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Component("helloBean")
     static class HelloBean {
 
